43dc4760a051eb5bed533599c152e061ee32fd42
Anton Medgaus <Anton_Medgaus@epam.com>
2020-07-28 23:02:56 +0600

    Init

--- Navigator_Solution/Navigator/Program.cs
+++ Navigator_Solution/Navigator/Program.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Navigator
 {
+    class FileSystemVisitor
+    {
+        //public event Action OnFileFound { add; remove; }
+        //public event Action OnDirectoryFound { add; remove; }
+
+        public List<string> GetFiles(Func<FileSystemInfo, bool> condition, FileSystemInfo dirFiles)
+        {
+            var result = new List<string>();
+            //logic for looping through files
+            try
+            {
+                //GetFiles(dirFiles => condition(dirFiles))
+                foreach (string d in Directory.GetDirectories(dirFiles.ToString()))
+                {
+                    if (condition(new DirectoryInfo(d)))
+                    {
+                        result.Add(d);
+                        //OnDirectoryFound();
+                        Console.WriteLine(d);
+                    }
+                    GetFiles(d => condition(d), new DirectoryInfo(d));
+                }
+                foreach (string f in Directory.GetFiles(dirFiles.ToString()))
+                {
+                    if (condition(new FileInfo(f)))
+                    {
+                        result.Add(f);
+                            //OnFileFound(); 
+                        Console.WriteLine(f);
+                    }
+                }
+                    
+                
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+            return result;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var visitor = new FileSystemVisitor();
+            //visitor.OnFileFound += () => Console.WriteLine("file found");
+            var result = visitor.GetFiles(f => f.Name.Length > 5, new DirectoryInfo(args[0]));
         }
     }
 }
